3830a2224fe22388d6ea3f0f7b0abbf5c58b6635
albertomorenogonzalez <albertomorenogonzalez03@gmail.com>
2024-01-24 12:05:42 +0100

    Styles and Comments

--- src/app.py
+++ src/app.py
@@ -1,20 +1,41 @@
+# Library imports
 import streamlit as st
 import numpy as np
+
+# Neuron class import
+# The class is located in another file in this directory
+# The name of the file is neuron.py
 from neuron import Neuron
 
+# We set the name of the page in the top of the tap
 st.set_page_config(page_title="Simulador de Neurona")
+# st.set_page_config(layout="wide")
 
+# We set the style of the page, first in a variable
+# and then applied with an st.markdown.
+style = f"""
+<style>
+    .appview-container .main .block-container{{
+        max-width: 90%
+    }}
+</style>
+"""
+
+st.markdown(style, unsafe_allow_html=True)
+
+# We set the image and title
 st.image("media/neurons.jpg", use_column_width="always")
 
 st.title('Simulador de Neurona')
 
+# We set the slider to show which number of entries and weights we want
 entry_num = st.slider("Elige el número de entradas/pesos que tendrá la neurona", 1, 10)
 
+# Setting the weights depending of the number we set before
 st.subheader("Pesos")
 weights = []
 
 weight_columns = st.columns(entry_num)
-
 
 for i in range(0, entry_num):
 
@@ -25,6 +46,7 @@ for i in range(0, entry_num):
 st.write(f"w = {weights}")
 
 
+# Setting the entries depending of the number we set before
 st.subheader("Entradas")
 entries = []
 
@@ -39,8 +61,8 @@ for i in range(0, entry_num):
 st.write(f"x = {entries}")
 
 
+# We set the bias and activation function input
 colBias, colActivation = st.columns(2)
-
 
 with colBias:
     st.subheader("Sesgo")
@@ -54,11 +76,12 @@ with colActivation:
     )
 
 
-# Instancia de la neurona y cálculos
+# Neuron instance and operations
 n = Neuron(weights, bias, function)
 output = n.predict(entries)
 
 
+# We set the button to run and show the results
 if st.button("Calcular la salida"):
         st.write(f"La salida de la neurona es {output}")
 
